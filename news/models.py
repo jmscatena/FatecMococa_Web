d9fb4d5c4d65714bd11aa8167b392c877cbaf8ed
jmscatena <jean.scatena@fatec.sp.gov.br>
2023-10-26 16:49:44 -0300

    update: atualizacao dos conteudos principais

---
+++ news/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class noticias(models.Model):
+    titulo = models.CharField(max_length=70, blank=False,null=False)
+    descricao = models.CharField(max_length=250,)
+    arquivo = models.FileField(upload_to='files/')
+    capa = models.ImageField(upload_to='imagens/news/')
