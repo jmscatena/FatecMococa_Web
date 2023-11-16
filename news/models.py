2f955026ce9b3f7d8cb88a83e007287a21927306
jmscatena <jean.scatena@fatec.sp.gov.br>
2023-11-16 14:30:24 -0300

    upd: news

--- news/models.py
+++ news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Noticia(models.Model):
     titulo = models.CharField(max_length=70, blank=False,null=False)
-    descricao = models.CharField(max_length=250,)
+    descricao = models.TextField()
     arquivo = models.FileField(upload_to='files/')
     capa = models.ImageField(upload_to='imagens/news/')
     data_publicacao = models.DateField(auto_now_add=True)
